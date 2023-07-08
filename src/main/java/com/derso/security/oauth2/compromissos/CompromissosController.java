5597cbdaa87b73f5bd1838b94b0fcd811167c128
Éderson Cássio <edersoncassio1983@gmail.com>
2023-07-08 16:57:47 -0300

    Lembretes para criar as telas com as funcionalidades. Essa parte dá preguiça, já tenho referência nos outros repositórios.

---
+++ src/main/java/com/derso/security/oauth2/compromissos/CompromissosController.java
@@ -0,0 +1,10 @@
+package com.derso.security.oauth2.compromissos;
+
+import org.springframework.stereotype.Controller;
+
+@Controller
+public class CompromissosController {
+
+	// TODO fazer algo aqui hahaha
+	
+}
---
+++ src/main/java/com/derso/security/oauth2/usuarios/UsuariosController.java
@@ -0,0 +1,10 @@
+package com.derso.security.oauth2.usuarios;
+
+import org.springframework.stereotype.Controller;
+
+@Controller
+public class UsuariosController {
+
+	// TODO fazer algo aqui
+	
+}
